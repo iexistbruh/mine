643b516309339e2af3d3ad30157e478de9a0205a
iexistbruh <farwahrumisa@gmail.com>
2024-10-03 18:49:35 +0600

    initial commit

---
+++ canvas star.py
@@ -0,0 +1,30 @@
+import turtle
+
+turtle.Screen().bgcolor("Green")
+board= turtle.Turtle()
+
+#Triangle
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+#Square
+board.forward(90)
+board.left(100)
+
+board.forward(90)
+board.left(100)
+
+board.forward(90)
+board.left(100)
+
+board.forward(90)
+board.left(100)
+
+
+turtle.title("Welcome To Turtle Window")
+turtle.mainloop()
